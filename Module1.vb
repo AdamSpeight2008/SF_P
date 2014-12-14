eb4a24d5beb8e59532773720a95badcf724a9020
AdamSpeight2008 <Adam.Speight@virgin.net>
2014-12-14 15:57:03 +0000

    Improved handling of Malformed FormatStrings

--- Module1.vb
+++ Module1.vb
@@ -9,14 +9,60 @@ Module Module1
   Sub Main()
     '0123456789012
 
-    Dim s = " {8 ,1:C2} {}}"
+    Dim ss = {
+      "",
+      "A",
+      "}",
+      "{",
+      "{{",
+      "}}",
+      "{0}",
+      "{A}",
+      "{0A}",
+      "{ 0}",
+      "{0 }",
+      "{0,}",
+      "{0,1}",
+      "{0,-1}",
+      "{0, 1}",
+      "{0, -1}",
+      "{0,A}",
+      "{0:{",
+      "{0:{{",
+      "{0:{{{",
+      "{0:{{}",
+      "{0:}}",
+      "{0:}}}",
+      "{0:}",
+      "{0,1:C2",
+      "{0,1:C2}",
+      "} X::= {0,9:C1}{0h}"
+      }
+    '    Dim s = " {9j}"
     'Dim r0 = ""
     'Try
     '  r0 = String.Format(s, 1.2345, 2.45)
     'Catch ex As Exception
     '  Console.WriteLine(ex)
     'End Try
-    Dim r1 = Type2_Parser.Parse(SourceText.Create(s))
+    For Each s In ss
+      Dim r As SpanKind = Type2_Parser.Parse(SourceText.Create(s))
+      Console.ForegroundColor = ConsoleColor.Red
+      Console.Write("{0}", s)
+      Console.ForegroundColor = ConsoleColor.White
+      Console.WriteLine(" {1}", s, r)
+      For Each p In r.MadeOf
+        Console.ForegroundColor = ConsoleColor.Green
+        Console.WriteLine("  {0}", p)
+        For Each pp In p.MadeOf
+          Console.ForegroundColor = ConsoleColor.Blue
+          Console.WriteLine("    {0}", pp)
+
+        Next
+      Next
+      Console.WriteLine()
+      Console.ReadKey()
+    Next
     'Dim r1 = StringFormat.Parse(s)
     'Dim i = 0
     'Dim r2 = r1.MadeOf(0)(1).GetValue(i)
--- SFD.StringFormat/Type2_Parser.vb
+++ SFD.StringFormat/Type2_Parser.vb
@@ -3,7 +3,7 @@
 
 
   Function Quoted(fs As SourceText, i As Integer) As Boolean
-    Return fs(i).Equals( fs(i + 1))
+    Return fs(i).Equals(fs(i + 1))
   End Function
   Public Function Parse(sr As SourceText) As SpanKind
     Dim spans As New List(Of SpanKind)
@@ -20,11 +20,13 @@
           Dim si = i
           InHole = True
           Dim arg_Hole = Parse_ArgHole(sr, i)
-          If arg_Hole.HasError Then
-              InErrorState = True
-            spans.Add( SpanKind.MakeFrom( StringFormat.Kinds.Err_Malformed_ArgHole,sr,si,i  ) )
-            Else
           spans.Add(arg_Hole)
+          If arg_Hole.HasError Then
+            InErrorState = True
+            'spans.Add(SpanKind.MakeFrom(StringFormat.Kinds.Err_Malformed_ArgHole, sr, si, i, {arg_Hole}))
+          Else
+            'spans.Add(arg_Hole)
+
           End If
 
           If Not InErrorState Then InHole = False
@@ -34,14 +36,15 @@
           IsQuoted = True
           spans.Add(SpanKind.MakeFrom(StringFormat.Kinds.QBR, sr, i, i + 2))
 
-        Case (sr(i) = "}"c)
-          If Not InHole Then
-            ' Parsing Error: Mismatched brace.
-            spans.Add(SpanKind.MakeFrom(StringFormat.Kinds.Err_UC, sr, i, i + 1))
-          Else
+        Case (sr(i) = "}"c) AndAlso Not InHole
+          ' Parsing Error: Mismatched 
+          spans.Add(SpanKind.MakeFrom(StringFormat.Kinds.Err_UC, sr, i, i + 1))
+        Case (sr(i) = "}"c) AndAlso InHole 
+            Dim item = spans.RemoveLast()
+            Dim mfh = SpanKind.MakeFrom(StringFormat.Kinds.Err_Malformed_ArgHole, sr, item.Start, i + 1, item.MadeOf)
+            spans.Add(mfh)
             InHole = False
             InErrorState = False
-          End If
       End Select
       If IsQuoted Then
         i += 1
@@ -52,65 +55,86 @@
     Return SpanKind.MakeFrom(StringFormat.Kinds.FormatString, sr, 0, i, spans)
   End Function
 
-  Private Function Parse_Arg_Index(sr As SourceText,
-                              ByRef i As Integer) As SpanKind
-    Dim si=i
- Dim Digits As SpanKind
-    If sr(i).IsDigit Then
-      'If sr(i).IsDigit = False Then
-      '  Return SpanKind.MakeFrom(StringFormat.Kinds.Err_Malformed_ArgIndex, sr, i, i + 1, {SpanKind.MakeFrom(StringFormat.Kinds.Err_UC, sr, i, i + 1)})
-      'End If
-      Digits = sr.Parse_Digits(i)
-      Dim RawText = Digits.GetText
-      Dim value = 0
-      If Integer.TryParse(RawText, value) Then Return SpanKind.MakeFrom(StringFormat.Kinds.Arg_Index, sr, si, si, {Digits})
-    End If
-    Return SpanKind.MakeFrom(StringFormat.Kinds.Err_Malformed_ArgIndex,sr, si, i, {Digits})
+  <Extension>
+  Function RemoveLast(Of T As class)(ByRef l As List(Of T)) As T
+    If l Is Nothing Then Return Nothing
+    Dim lastIndex = l.Count-1
+    Dim lastItem =l(lastIndex)
+    l.RemoveAt(lastIndex)
+    Return lastItem
   End Function
 
-  Private Function Parse_Arg_Alignment(sr As SourceText,
+  Private Function Parse_Arg_Index(sr As SourceText,
                               ByRef i As Integer) As SpanKind
-    Dim Raw = ""
     Dim si = i
+    Dim parts As New List(Of SpanKind)
+    If sr(i).IsDigit Then
+      Dim Digits = sr.Parse_Digits(i)
+      parts.Add(digits)
+      If i >= sr.Length Then Return UnepectedEOT(StringFormat.Kinds.Err_Malformed_ArgIndex, sr, si, Digits.Finish+1, parts)
+
+      Dim RawText = Digits.GetText
+      Dim value = 0
+      'If Integer.TryParse(RawText, value) Then
+      Return SpanKind.MakeFrom(StringFormat.Kinds.Arg_Index, sr, si, Digits.Finish, parts)
+
+    End If
+    Return SpanKind.MakeFrom(StringFormat.Kinds.Err_Malformed_ArgIndex, sr, si, i+1,parts)
+  End Function
+
+  Private Function Parse_Arg_Alignment(sr As SourceText, ByRef i As Integer) As SpanKind
+    Dim si = i
+    Dim spaces = Consume_Spaces(sr, i)
+    'Dim Raw = ""
+    Dim parts As New List(Of SpanKind)
     Dim minus As SpanKind = Nothing
     If sr(i) = "-"c Then
-      minus = SpanKind.MakeFrom(StringFormat.Kinds.Minus,sr , i, i + 1)
+      minus = SpanKind.MakeFrom(StringFormat.Kinds.Minus, sr, i, i + 1)
+      parts.Add(minus)
       i += 1
     End If
     If sr(i).IsDigit Then
       Dim digits = sr.Parse_Digits(i)
-      Return SpanKind.MakeFrom(StringFormat.Kinds.Arg_Align, sr, si, i, {minus, digits})
+      parts.Add(digits)
+      Return SpanKind.MakeFrom(StringFormat.Kinds.Arg_Align, sr, si, i, parts)
     Else
-      Return SpanKind.MakeFrom(StringFormat.Kinds.Err_UC,sr , i, i + 1)
+      If i >= sr.Length Then Return UnepectedEOT(StringFormat.Kinds.Err_Malformed_ArgAlign,sr,si,i, parts)
+      'Dim uec = SpanKind.MakeFrom(StringFormat.Kinds.Err_UC, sr, i, i + 1)
+      'parts(uec)
+      ' Return UnepectedEOT(StringFormat.Kinds.Err_Malformed_ArgAlign,sr,si,i, parts)
+      Return SpanKind.MakeFrom(StringFormat.Kinds.Err_Malformed_ArgAlign, sr, si, i + 1, parts)
     End If
   End Function
 
 
-  Private Function Parse_Arg_Format(sr As SourceText,
-                              ByRef i As Integer) As SpanKind
+  Private Function Parse_Arg_Format(sr As SourceText, ByRef i As Integer) As SpanKind
     Dim si = i
-    Dim EoAF = False 
+    Dim parts As New List(Of SpanKind)
     While (i < sr.Length)
-      Dim c = sr(i) 
-      Dim q = Quoted(sr,i)
+      Dim c = sr(i)
+      Dim q = Quoted(sr, i)
       If (c = "{"c) Then
         If q Then
           i += 1
         Else
-          Dim uc = SpanKind.MakeFrom(StringFormat.Kinds.BL,sr,i,i+1)
-          Return SpanKind.MakeFrom(StringFormat.Kinds.Err_Malformed_ArgFormat, sr, si, i, {uc})
+          Return UnepectedEOT(StringFormat.Kinds.Err_Malformed_ArgFormat,sr,si,i+1,parts)
+          'Dim uc = SpanKind.MakeFrom(StringFormat.Kinds.BL, sr, i, i + 1)
+          ' Return SpanKind.MakeFrom(StringFormat.Kinds.Err_Malformed_ArgFormat, sr, si, i, {uc})
         End If
-      ElseIf (c="}"c) Then
+      ElseIf (c = "}"c) Then
         If q Then
-          i+=1
+          i += 1
         Else
-          Dim uc = SpanKind.MakeFrom(StringFormat.Kinds.BR, sr, i, i + 1)
-          Return SpanKind.MakeFrom(StringFormat.Kinds.Err_Malformed_ArgFormat, sr, si, i, {uc})
+          Exit While 
+          'Dim uc = SpanKind.MakeFrom(StringFormat.Kinds.BR, sr, i, i + 1)
+          'Return SpanKind.MakeFrom(StringFormat.Kinds.Err_Malformed_ArgFormat, sr, si, i, {uc})
 
         End If
       End If
       i += 1
     End While
+    If i >= sr.Length Then Return UnepectedEOT(StringFormat.Kinds.Err_Malformed_ArgFormat, sr, si, i+1,parts)
+
     Return SpanKind.MakeFrom(StringFormat.Kinds.Arg_Format, sr, si, i)
   End Function
 
@@ -120,49 +144,73 @@
                               ByRef i As Integer) As SpanKind
     Dim LeftEdgeOfHole = i
     Dim parts As New List(Of SpanKind)
-    parts.Add(SpanKind.MakeFrom(StringFormat.Kinds.BR,sr,i,i+1))
+    parts.Add(SpanKind.MakeFrom(StringFormat.Kinds.BR, sr, i, i + 1))
     i += 1
-    If i >= sr.Length Then Return SpanKind.MakeFrom(StringFormat.Kinds.Err_EOT, sr, i - 1, i)
+    If i >= sr.Length Then Return UnepectedEOT(sr, LeftEdgeOfHole,i,  parts)
 
     Dim Index_Value As SpanKind = Nothing
     Dim Align_Value As SpanKind = Nothing
     Dim Format_Value As SpanKind = Nothing
     If sr(i).IsDigit Then
       Index_Value = Parse_Arg_Index(sr, i)
-      'If Index_Value.HasError Then Return res
       parts.Add(Index_Value)
+      If Index_Value.HasError Then Return SpanKind.MakeFrom(StringFormat.Kinds.Err_Malformed_ArgHole, sr, LeftEdgeOfHole, i+1, parts)
     Else
       ' Add parser for identifers here
-
     End If
-    Dim si=i
+    Dim si = i
     sr.Consume_Spaces(i)
-    If (si-i) >0 Then parts.Add(SpanKind.MakeFrom(StringFormat.Kinds.Spaces,sr,si,i+1))
+    If (si - i) > 0 Then parts.Add(SpanKind.MakeFrom(StringFormat.Kinds.Spaces, sr, si, i + 1))
     ' Alignment 
     If sr(i) = ","c Then
-      parts.Add(SpanKind.MakeFrom(StringFormat.Kinds.Comma,sr,i,i+1))
+      parts.Add(SpanKind.MakeFrom(StringFormat.Kinds.Comma, sr, i, i + 1))
       i += 1
-      If (i >= sr.Length) Then Return SpanKind.MakeFrom(StringFormat.Kinds.Err_EOT,sr , i - 1, i)
-       Align_Value = Parse_Arg_Alignment(sr, i)
-       parts.Add(Align_Value)
-      'If Align_Value.HasError Then
-      'Else
-      '  Align_Value = res1
-      'End If
+      If i >= sr.Length Then Return UnepectedEOT(sr,  LeftEdgeOfHole,i, parts)
+      Align_Value = Parse_Arg_Alignment(sr, i)
+      parts.Add(Align_Value)
+      If Align_Value.HasError Then
+          Return SpanKind.MakeFrom(StringFormat.Kinds.Err_Malformed_ArgHole, sr, LeftEdgeOfHole, i+1, parts)
+      End If
     End If
     ' Formatting
     If sr(i) = ":"c Then
-      parts.Add(SpanKind.MakeFrom(StringFormat.Kinds.Colon,sr,i,i+1))
+      parts.Add(SpanKind.MakeFrom(StringFormat.Kinds.Colon, sr, i, i + 1))
       i += 1
       Format_Value = Parse_Arg_Format(sr, i)
       parts.Add(Format_Value)
+      If Format_Value.HasError Then
+        Return SpanKind.MakeFrom(StringFormat.Kinds.Err_Malformed_ArgHole, sr, LeftEdgeOfHole, i+1, parts)
+      End If
+
     ElseIf sr(i) = "}"c Then
 
     Else
-      Return SpanKind.MakeFrom(StringFormat.Kinds.Err_UC,sr , i, i + 1)
+      If i >= sr.Length Then Return UnepectedEOT(sr, LeftEdgeOfHole, i, parts)
+
+      Return UnexpectedChar(sr, LeftEdgeOfHole, i, parts)
     End If
     parts.Add(SpanKind.MakeFrom(StringFormat.Kinds.BR, sr, i, i + 1))
     Return SpanKind.MakeFrom(StringFormat.Kinds.ArgHole, sr, LeftEdgeOfHole, i + 1, parts)
+  End Function
+
+  Private Function UnexpectedChar(sr As SourceText, si As Integer, i As Integer, ByRef parts As List(Of SpanKind)) As SpanKind
+    Return UnexpectedChar(StringFormat.Kinds.Err_Malformed_ArgHole, sr, si, i, parts)
+  End Function
+
+  Private Function UnepectedEOT(sr As SourceText, si As Integer, i As Integer, ByRef parts As List(Of SpanKind)) As SpanKind
+    Return UnepectedEOT(StringFormat.Kinds.Err_Malformed_ArgHole, sr, si, i, parts)
+  End Function
+
+  Private Function UnexpectedChar(sk As StringFormat.Kinds, sr As SourceText,si As Integer, i As Integer,  ByRef parts As List(Of SpanKind)) As SpanKind
+    'If si > i Then Throw New Exception
+    parts.Add(SpanKind.MakeFrom(StringFormat.Kinds.Err_UC, sr, si, i + 1))
+    Return SpanKind.MakeFrom(sk, sr, si, i+2, parts)
+  End Function
+
+  Private Function UnepectedEOT(sk As StringFormat.Kinds, sr As SourceText, si As Integer, i As Integer, ByRef parts As List(Of SpanKind)) As SpanKind
+    'If si>i Then Throw New Exception 
+    parts.Add(SpanKind.MakeFrom(StringFormat.Kinds.Err_EOT, sr, i-1, i))
+    Return SpanKind.MakeFrom(sk, sr, si, i + 1, parts)
   End Function
 
   <Runtime.CompilerServices.Extension>
@@ -176,7 +224,7 @@
     While i < sr.Length AndAlso sr(i).IsDigit
       i += 1
     End While
-    Return SpanKind.MakeFrom(If((i - si) = 0, StringFormat.Kinds.Empty, StringFormat.Kinds.Digits), sr,si, i )
+    Return SpanKind.MakeFrom(If((i - si) = 0, StringFormat.Kinds.Empty, StringFormat.Kinds.Digits), sr, si, i)
   End Function
 
   <Runtime.CompilerServices.Extension>
